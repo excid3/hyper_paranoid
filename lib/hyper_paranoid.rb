97974657b06a7def883256162e732109aa75865d
Chris Oliver <excid3@gmail.com>
2013-05-08 20:43:08 -0500

    Add initializer to require acts_as_paranoid in the rails app

--- lib/hyper_paranoid.rb
+++ lib/hyper_paranoid.rb
@@ -2,6 +2,10 @@ require "hyper_paranoid/version"
 
 module HyperParanoid
   class Railtie < Rails::Railtie
+    initializer "require stuff" do
+      require "acts_as_paranoid"
+    end
+
     config.app_generators do |g|
       g.templates.unshift File::expand_path("../templates", __FILE__)
     end
